c85b4baa73c06af36755eaa1822d9197f329244b
Backl1ght <1229309527@qq.com>
2020-10-11 19:43:01 +0800

    add codeforces edu round 96 ABCDE

---
+++ Codeforces/Educational Codeforces Round 96 (Rated for Div. 2)/A.cpp
@@ -0,0 +1,232 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// #include <ext/rope>
+// using namespace __gnu_cxx;
+ 
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+ 
+// typedef ll key_type;
+// typedef null_mapped_type value_type;
+// typedef tree<key_type, value_type, less<key_type>, rb_tree_tag, tree_order_statistics_node_update> rbtree;
+ 
+// typedef __gnu_pbds::priority_queue<pi,greater<pi>,pairing_heap_tag > heap;
+ 
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// int rnd(int l,int r){return l+rng()%(r-l+1);}
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> PII;
+typedef vector<int> VI;
+typedef vector<vector<int>> VVI;
+ 
+#define REP(i, _, __) for (int i = (_); i < (__); ++i)
+#define PER(i, _, __) for (int i = (_-1); i >= (__); --i)
+#define FOR(i, _, __) for (int i = (_); i <= (__); ++i)
+#define ROF(i, _, __) for (int i = (_); i >= (__); --i)
+
+#define EB emplace_back
+#define PB push_back
+#define MP make_pair
+#define FI first
+#define SE second
+#define SZ(x) ((int)(x).size())
+#define ALL(x) (x).begin(),(x).end()
+#define LLA(x) (x).rbegin(),(x).rend()
+
+const double pi = acos(-1.0);
+   
+namespace Backlight {
+    bool REOF = 1; //为0表示文件结尾
+    inline char nc() {
+        static char buf[1 << 20], *p1 = buf, *p2 = buf;
+        return p1 == p2 && REOF && (p2 = (p1 = buf) + fread(buf, 1, 1 << 20, stdin), p1 == p2) ? (REOF = 0, EOF) : *p1++;
+    }
+   
+    template<typename T>
+    inline bool read(T &x) {
+        char c = nc();
+        bool f = 0; x = 0;
+        while (c < '0' || c > '9') c == '-' && (f = 1), c = nc();
+        while (c >= '0'&&c <= '9') x = (x << 3) + (x << 1) + (c ^ 48), c = nc();
+        if (f) x = -x;
+        return REOF;
+    }
+ 
+    template<typename T>
+    inline void println(T x, bool flag = true) {
+        if (x < 0) putchar('-'), x = -x;
+        if(x > 9) println(x / 10, false);
+        putchar('0' + x % 10);
+        if (flag) putchar('\n');
+    }
+
+    #define YES puts("YES");
+    #define NO puts("NO");
+    #define Yes puts("Yes");
+    #define No puts("No");
+    #define yes puts("yes");
+    #define no puts("no");
+   
+    template<typename T, typename... T2>
+    inline bool read(T &x, T2 &... rest) {
+        read(x);
+        return read(rest...);
+    }
+   
+   inline bool isdowncase(char c) { return (c >= 'a') && (c <= 'z'); }
+   inline bool isupcase(char c) { return (c >= 'A') && (c <= 'Z'); }
+   inline bool isdigit(char c) { return (c >= '0') && (c <= '9'); }
+
+    // inline bool need(char &c) { return (c == '.') || (c == 'x') || (c=='o');}
+    // inline bool need(char &c) { return isdowncase(c) || isupcase(c) || isdigit(c) || c == '.' || c == '#'; }
+    inline bool need(char &c) { return isdowncase(c) || isupcase(c) || isdigit(c); }
+
+    inline bool read_str(char *a) {
+        while ((*a = nc()) && need(*a) && REOF) ++a; *a = '\0';
+        return REOF;
+    }
+   
+    inline bool read_db(double &x){
+        bool f = 0; char ch = nc(); x = 0;
+        while (ch < '0' || ch > '9')  { f |= (ch == '-'); ch = nc(); }
+        while (ch >= '0' && ch <= '9') { x = x * 10.0 + (ch^48); ch = nc(); }
+        if(ch == '.') {
+            double temp = 1; ch = nc();
+            while (ch >= '0' && ch <= '9') { temp = temp / 10.0; x = x + temp * (ch^48); ch = nc(); }
+        }
+        if(f) x = -x;
+        return REOF;
+    }
+   
+    template<typename TH>
+    inline void _dbg(const char *sdbg, TH h){ cerr << sdbg << '=' << h << endl; }
+   
+    template<typename TH, typename... TA>
+    inline void _dbg(const char *sdbg, TH h, TA... a) {
+        while(*sdbg != ',') cerr << *sdbg++;
+        cerr << '=' << h << ", ";
+        _dbg(sdbg+1, a...);
+    }
+      
+    template<typename T>
+    ostream &operator<<(ostream& os, vector<T> V) {
+        os << "[ "; for (auto v : V) os << v << ","; return os << " ]";
+    }
+   
+    template<typename T>
+    ostream &operator<<(ostream& os, set<T> V) {
+        os << "[ "; for (auto v : V) os << v << ","; return os << " ]";
+    }
+
+    template<typename T>
+    ostream &operator<<(ostream& os, multiset<T> V) {
+        os << "[ "; for (auto v : V) os << v << ","; return os << " ]";
+    }
+ 
+    template<typename T1, typename T2>
+    ostream &operator<<(ostream& os, map<T1,T2> V) {
+        os << "[ "; for (auto v : V) os << v << ","; return os << " ]";
+    }
+  
+    template<typename L, typename R>
+    ostream &operator<<(ostream &os, pair<L,R> P) {
+        return os << "(" << P.first << "," << P.second << ")";
+    }
+
+
+    template<typename T>
+    T MIN(T a, T b) { return min(a, b); }
+
+    template<typename First, typename... Rest>
+    First MIN(First f, Rest... r) { return min(f, MIN(r...)); }
+
+    template<typename T>
+    T MAX(T a, T b) { return max(a, b); }
+
+    template<typename First, typename... Rest>
+    First MAX(First f, Rest... r) { return max(f, MAX(r...)); }
+
+    template<typename T>
+    void updMin(T& a, T b) { if (a > b) a = b; }
+
+    template<typename T>
+    void updMax(T& a, T b) { if (a < b) a = b; }
+ 
+    #ifdef BACKLIGHT
+    #define debug(...) _dbg(#__VA_ARGS__, __VA_ARGS__)
+    #else
+    #define debug(...)
+    #endif
+}
+
+/************     Backlight     **********
+ * 一发入魂
+ * 仔细读题，注意边界条件
+ * 没有思路就试试逆向思维
+ * wdnmd!我柜子动了不打了
+ * 能不能把我掉的分还给我
+ ************     Backlight     **********/
+
+using namespace Backlight;
+const int N = 5e5 + 5;
+const int M = 5e5 + 5;
+const int V = 5e7 + 5;
+const ll MOD = 998244353;              // 998244353 1e9+7
+const int INF = 0x3f3f3f3f;             // 1e9+7 0x3f3f3f3f
+const ll LLINF = 0x3f3f3f3f3f3f3f3f;    // 1e18+9 0x3f3f3f3f3f3f3f3f
+const double eps = 1e-8;
+
+int n, a[1005], b[1005], c[1005];
+void init() {
+    memset(a, -1, sizeof(a));
+    memset(b, -1, sizeof(b));
+    memset(c, -1, sizeof(c));
+
+    a[3] = 1; b[3] = 0; c[3] = 0;
+    a[5] = 0; b[5] = 1; c[5] = 0;
+    a[7] = 0; b[7] = 0; c[7] = 1;
+
+    for (int i = 1; i <= 1000; ++i) {
+        if (i > 3 && a[i - 3] != -1) {
+            a[i] = a[i - 3] + 1;
+            b[i] = b[i - 3]; 
+            c[i] = c[i - 3];
+            continue;
+        }
+        if (i > 5 && a[i - 5] != -1) {
+            a[i] = a[i - 5];
+            b[i] = b[i - 5] + 1; 
+            c[i] = c[i - 5];
+            continue;
+        }
+        if (i > 7 && a[i - 7] != -1) {
+            a[i] = a[i - 7];
+            b[i] = b[i - 7]; 
+            c[i] = c[i - 7] + 1;
+            continue;
+        }
+    }
+}
+
+void solve(int Case) { // printf("Case #%d: ", Case);
+    read(n);
+    if (a[n] == -1) puts("-1");
+    else printf("%d %d %d\n", a[n], b[n], c[n]);
+}
+
+int main()
+{
+#ifdef BACKLIGHT
+    freopen("in.txt", "r", stdin);
+#endif
+    init();
+    // ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int T = 1;
+    read(T);
+    for (int _ = 1; _ <= T; _++) solve(_);
+    return 0;
+}
---
+++ Codeforces/Educational Codeforces Round 96 (Rated for Div. 2)/E.cpp
@@ -0,0 +1,236 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// #include <ext/rope>
+// using namespace __gnu_cxx;
+ 
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+ 
+// typedef ll key_type;
+// typedef null_mapped_type value_type;
+// typedef tree<key_type, value_type, less<key_type>, rb_tree_tag, tree_order_statistics_node_update> rbtree;
+ 
+// typedef __gnu_pbds::priority_queue<pi,greater<pi>,pairing_heap_tag > heap;
+ 
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// int rnd(int l,int r){return l+rng()%(r-l+1);}
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> PII;
+typedef vector<int> VI;
+typedef vector<vector<int>> VVI;
+ 
+#define REP(i, _, __) for (int i = (_); i < (__); ++i)
+#define PER(i, _, __) for (int i = (_-1); i >= (__); --i)
+#define FOR(i, _, __) for (int i = (_); i <= (__); ++i)
+#define ROF(i, _, __) for (int i = (_); i >= (__); --i)
+
+#define EB emplace_back
+#define PB push_back
+#define MP make_pair
+#define FI first
+#define SE second
+#define SZ(x) ((int)(x).size())
+#define ALL(x) (x).begin(),(x).end()
+#define LLA(x) (x).rbegin(),(x).rend()
+
+const double pi = acos(-1.0);
+   
+namespace Backlight {
+    bool REOF = 1; //为0表示文件结尾
+    inline char nc() {
+        static char buf[1 << 20], *p1 = buf, *p2 = buf;
+        return p1 == p2 && REOF && (p2 = (p1 = buf) + fread(buf, 1, 1 << 20, stdin), p1 == p2) ? (REOF = 0, EOF) : *p1++;
+    }
+   
+    template<typename T>
+    inline bool read(T &x) {
+        char c = nc();
+        bool f = 0; x = 0;
+        while (c < '0' || c > '9') c == '-' && (f = 1), c = nc();
+        while (c >= '0'&&c <= '9') x = (x << 3) + (x << 1) + (c ^ 48), c = nc();
+        if (f) x = -x;
+        return REOF;
+    }
+ 
+    template<typename T>
+    inline void println(T x, bool flag = true) {
+        if (x < 0) putchar('-'), x = -x;
+        if(x > 9) println(x / 10, false);
+        putchar('0' + x % 10);
+        if (flag) putchar('\n');
+    }
+
+    #define YES puts("YES");
+    #define NO puts("NO");
+    #define Yes puts("Yes");
+    #define No puts("No");
+    #define yes puts("yes");
+    #define no puts("no");
+   
+    template<typename T, typename... T2>
+    inline bool read(T &x, T2 &... rest) {
+        read(x);
+        return read(rest...);
+    }
+   
+   inline bool isdowncase(char c) { return (c >= 'a') && (c <= 'z'); }
+   inline bool isupcase(char c) { return (c >= 'A') && (c <= 'Z'); }
+   inline bool isdigit(char c) { return (c >= '0') && (c <= '9'); }
+
+    // inline bool need(char &c) { return (c == '.') || (c == 'x') || (c=='o');}
+    // inline bool need(char &c) { return isdowncase(c) || isupcase(c) || isdigit(c) || c == '.' || c == '#'; }
+    inline bool need(char &c) { return isdowncase(c) || isupcase(c) || isdigit(c); }
+
+    inline bool read_str(char *a) {
+        while ((*a = nc()) && need(*a) && REOF) ++a; *a = '\0';
+        return REOF;
+    }
+   
+    inline bool read_db(double &x){
+        bool f = 0; char ch = nc(); x = 0;
+        while (ch < '0' || ch > '9')  { f |= (ch == '-'); ch = nc(); }
+        while (ch >= '0' && ch <= '9') { x = x * 10.0 + (ch^48); ch = nc(); }
+        if(ch == '.') {
+            double temp = 1; ch = nc();
+            while (ch >= '0' && ch <= '9') { temp = temp / 10.0; x = x + temp * (ch^48); ch = nc(); }
+        }
+        if(f) x = -x;
+        return REOF;
+    }
+   
+    template<typename TH>
+    inline void _dbg(const char *sdbg, TH h){ cerr << sdbg << '=' << h << endl; }
+   
+    template<typename TH, typename... TA>
+    inline void _dbg(const char *sdbg, TH h, TA... a) {
+        while(*sdbg != ',') cerr << *sdbg++;
+        cerr << '=' << h << ", ";
+        _dbg(sdbg+1, a...);
+    }
+      
+    template<typename T>
+    ostream &operator<<(ostream& os, vector<T> V) {
+        os << "[ "; for (auto v : V) os << v << ","; return os << " ]";
+    }
+   
+    template<typename T>
+    ostream &operator<<(ostream& os, set<T> V) {
+        os << "[ "; for (auto v : V) os << v << ","; return os << " ]";
+    }
+
+    template<typename T>
+    ostream &operator<<(ostream& os, multiset<T> V) {
+        os << "[ "; for (auto v : V) os << v << ","; return os << " ]";
+    }
+ 
+    template<typename T1, typename T2>
+    ostream &operator<<(ostream& os, map<T1,T2> V) {
+        os << "[ "; for (auto v : V) os << v << ","; return os << " ]";
+    }
+  
+    template<typename L, typename R>
+    ostream &operator<<(ostream &os, pair<L,R> P) {
+        return os << "(" << P.first << "," << P.second << ")";
+    }
+
+
+    template<typename T>
+    T MIN(T a, T b) { return min(a, b); }
+
+    template<typename First, typename... Rest>
+    First MIN(First f, Rest... r) { return min(f, MIN(r...)); }
+
+    template<typename T>
+    T MAX(T a, T b) { return max(a, b); }
+
+    template<typename First, typename... Rest>
+    First MAX(First f, Rest... r) { return max(f, MAX(r...)); }
+
+    template<typename T>
+    void updMin(T& a, T b) { if (a > b) a = b; }
+
+    template<typename T>
+    void updMax(T& a, T b) { if (a < b) a = b; }
+ 
+    #ifdef BACKLIGHT
+    #define debug(...) _dbg(#__VA_ARGS__, __VA_ARGS__)
+    #else
+    #define debug(...)
+    #endif
+}
+
+/************     Backlight     **********
+ * 一发入魂
+ * 仔细读题，注意边界条件
+ * 没有思路就试试逆向思维
+ * wdnmd!我柜子动了不打了
+ * 能不能把我掉的分还给我
+ ************     Backlight     **********/
+
+using namespace Backlight;
+const int N = 5e5 + 5;
+const int M = 5e5 + 5;
+const int V = 1e6 + 5;
+const ll MOD = 998244353;              // 998244353 1e9+7
+const int INF = 0x3f3f3f3f;             // 1e9+7 0x3f3f3f3f
+const ll LLINF = 0x3f3f3f3f3f3f3f3f;    // 1e18+9 0x3f3f3f3f3f3f3f3f
+const double eps = 1e-8;
+
+int n;
+char s[N], t[N];
+
+VI pc[30];
+
+int b[N];
+inline int lb(int x) { return x & -x; }
+void add(int x, int d) { for (; x < N; x += lb(x)) b[x] += d; }
+int getsum(int x) { int r = 0; for (; x; x -= lb(x)) r += b[x]; return r; }
+
+void solve(int Case) { // printf("Case #%d: ", Case);
+    read(n);
+    read_str(s + 1);
+    FOR(i, 1, n) t[i] = s[i]; reverse(t + 1, t + 1 + n);
+
+    // debug(s + 1, t + 1);
+
+    FOR(i, 1, n) {
+        int c = s[i] - 'a';
+        pc[c].PB(i);
+    }
+
+    ll ans = 0, dst = n;
+    ROF(i, n, 1) {
+        int c = t[i] - 'a';
+
+        int from = pc[c].back(); 
+        pc[c].pop_back();
+
+        // debug(from, getsum(from - 1));
+
+        int st = from - getsum(from - 1);
+
+        // debug(t[i], st, dst, dst - st);
+
+        ans += dst - st;
+
+        add(from, 1); --dst;
+    }
+
+    println(ans);
+}
+
+int main()
+{
+#ifdef BACKLIGHT
+    freopen("in.txt", "r", stdin);
+#endif
+    // ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int T = 1;
+    // read(T);
+    for (int _ = 1; _ <= T; _++) solve(_);
+    return 0;
+}
